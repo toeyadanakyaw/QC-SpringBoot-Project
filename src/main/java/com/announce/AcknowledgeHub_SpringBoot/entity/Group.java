f149904a8446e2df380cb449bdb1dfd9e39faff1
Toe Yadana Kyaw <50312009+toeyadanakyaw@users.noreply.github.com>
2024-08-27 15:23:35 +0630

    initialize project

---
+++ src/main/java/com/announce/AcknowledgeHub_SpringBoot/entity/Group.java
@@ -0,0 +1,33 @@
+package com.announce.AcknowledgeHub_SpringBoot.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "`group`")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_user",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    @JsonManagedReference
+    private Set<User> users;
+
+}
